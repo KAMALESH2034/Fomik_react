8bbbecf1657dcb0f58279f41c21d78fd583b7f1b
KAMALESH2034 <kamaleshjeeva72@gmail.com>
2024-08-26 12:51:45 +0530

    submit added

---
+++ src/Form.jsx
@@ -0,0 +1,268 @@
+import React from 'react'
+import { useFormik } from 'formik'
+import { useEffect, useState } from 'react'
+import {Outlet, useNavigate} from 'react-router-dom'
+import axios from 'axios'
+function Form() {
+    const [geoState,setgeoState] = useState([])
+    const [geoCity,setgeoCity] = useState([])
+    const navigate = useNavigate()
+    const formik =useFormik({
+      initialValues : {
+        userName : "",
+        Email : "",
+        Country : "",
+        State : "",
+        City : "",
+        Address : ""
+      },
+      validate : (values) => {
+        let error ={}
+  
+        if(values.userName == "" || values.userName.length <3 || values.userName.length >20) {
+          error.userName = "Please enter Valid name";
+        }
+        if(values.Email == "" || !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.Email)) {
+          error.Email = "Please enter Your Email";
+        }
+        if(values.Country == "") {
+          error.Country = "Please enter Your Country";
+        }
+        if(values.State == "") {
+          error.State = "Please enter Your State";
+        }
+        if(values.City == "") {
+          error.City = "Please enter Your City";
+        }
+        if(values.Address == "") {
+          error.Address = "Please enter Your Address";
+        }
+  
+        return error
+        
+      },
+      onSubmit : async (values) => {
+        console.log(values)
+             
+        try{
+          const userData = await axios.post("https://66c9ac4f59f4350f064d1e89.mockapi.io/forms",values)
+         
+        } catch {
+          console.log(error)
+        }
+        navigate("/submited");
+      }
+       
+  
+    })
+  
+    let geoData = {
+      countries : [{
+        name : "India",
+        value : "IN"
+      },{
+        name : "America",
+        value : "US"
+      }
+    ],
+      states : [
+        {
+          name : "Tamil Nadu",
+          value : "TN",
+          Country : "IN"
+        },
+        {
+          name : "Kerala",
+          value : "KL",
+          Country : "IN"
+        },
+        {
+          name : "New York",
+           value : "NY",
+           Country : "US"
+        },
+        {
+          name : "California",
+           value : "CA",
+           Country : "US"
+        }
+      ],
+      cities : [{
+        name : "Chennai",
+         value : "CHN",
+         States : "TN"
+      },{
+        name : "Madurai",
+        value : "MDU",
+        States : "TN"
+      },{
+        name : "Coimbatore",
+        value : "CMB",
+        States : "TN"
+      },{
+        name : "Thiruvananthapuram",
+        value : "THP",
+        States : "KL"
+      },{
+        name : "Kollam",
+        value : "KLM",
+        States : "KL"
+      },{
+        name : "Kannur",
+        value : "KNR",
+        States : "KL"
+      },{
+        name : "New York",
+        value : "NYC",
+        States : "NY"
+      },{
+        name : "Los Angeles",
+        value : "LA",
+        States : "CA"
+      },{
+        name : "San Francisco",
+        value : "SF",
+        States : "CA"
+      },{
+        name : "San Diego",
+        value : "SD",
+        States : "CA"
+      }]
+    }
+  
+    useEffect(() =>{
+      let stateList = geoData.states.filter((state)=>{
+        return state.Country == formik.values.Country
+      })
+      setgeoState(stateList)
+    },[formik.values.Country])
+  
+    useEffect(() =>{
+      let cityList = geoData.cities.filter((city)=>{
+        return city.States == formik.values.State
+      })
+      setgeoCity(cityList)
+    },[formik.values.State])
+
+
+  return (
+    <>
+    <div className='container mt-4'>
+      <div className='row'>
+        <h2> GuluGulu Form</h2>
+        <form onSubmit={formik.handleSubmit}>
+            <div className='col-lg-12 mt-3'>
+              <div className='row'>
+                <div className='col-lg-7'>
+                  <label>Name</label>
+                  <input type='text' 
+                  placeholder='Enter Your Name'
+                  name = 'userName'
+                  value={formik.values.userName}
+                  onChange={formik.handleChange}
+                  className={`form-control  ${formik.errors.userName && 'is-invalid'}`} />
+                  <span style={{color : "maroon"}}>{formik.errors.userName}</span>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-12 mt-3'>
+              <div className='row'>
+                <div className='col-lg-7'>
+                <label>Email</label>
+                  <input type='text' 
+                  placeholder='abc@ymail.com'
+                  name = 'Email'
+                  value={formik.values.Email}
+                  onChange={formik.handleChange}
+                  className={`form-control ${formik.errors.Email && 'is-invalid'}`} />
+                   <span  style={{color : "maroon"}}>{formik.errors.Email}</span>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-lg-12 mt-3'>
+              <div className='row'>
+                <div className='col-lg-4'>
+                  <label>Country</label>
+                  <select 
+                  id="" 
+                  name = 'Country'
+                  value={formik.values.Country}
+                  onChange={formik.handleChange}
+                  className={`form-control   ${formik.errors.Country && 'is-invalid'}`}>
+                    <option value='nil'>Not Selected</option>
+                    <option value='IN'>India</option>
+                    <option value='US'>America</option>
+                  </select>
+                  <span style={{color : "maroon"}}>{formik.errors.Country}</span>
+                </div>
+                <div className='col-lg-4'>
+                  <label>state</label>
+                  <select 
+                  name = 'State'
+                  value={formik.values.State}
+                  onChange={formik.handleChange} id=""  
+                  className={`form-control ${formik.errors.State && 'is-invalid'}`}>
+                    <option value='nil'>Not Selected</option>
+                      {
+                        geoState.map((state)=>{
+                          return <option value={state.value}>{state.name}</option>
+                        })
+                      }
+                  </select>
+                  <span style={{color : "maroon"}}>{formik.errors.State}</span>
+                </div>
+                <div className='col-lg-4'>
+                  <label>City</label>
+                  <select 
+                  name = 'City'
+                  value={formik.values.City}
+                  onChange={formik.handleChange} id="" 
+                  className={`form-control ${formik.errors.City && 'is-invalid'}`}>
+                    <option value='nil'>Not Selected</option>
+                    {
+                      geoCity.map((city)=>{
+                        return <option value={city.value}>{city.name}</option>
+                      })
+                    }
+                  </select>
+                  <span style={{color : "maroon"}}>{formik.errors.City}</span>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-lg-12 mt-3'>
+              <div className='row'>
+                  <div className='col-lg-7'>
+                    <label>Address</label>
+                    <textarea 
+                    type='textbox' 
+                    placeholder='Enter Your Address'
+                    name = 'Address'
+                  value={formik.values.Address}
+                  onChange={formik.handleChange}
+                  className={`form-control mb-1 ${formik.errors.Address && 'is-invalid'}`} />
+                  <span  style={{color : "maroon"}}>{formik.errors.Address}</span>
+                  </div>
+              </div>
+            </div>
+
+            <div className='text-center mt-3 mb-4'>
+            
+            <button  type='submit'
+             className='btn btn-outline-primary btn-lg fw-bold px-4'>
+               Submit
+                
+             </button>
+           
+            </div>
+
+          </form>
+      </div>
+      <Outlet/>
+    </div>
+    </>
+  )
+}
+
+export default Form
---
+++ src/Submited.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+function Submited() {
+
+    const navigate = useNavigate()
+
+  return (
+    <div class="container ">
+       <div class="row">
+         <div className='d-flex justify-content-center'>
+         <div class="col-12 col-lg-5 mt-5 mb-8 mb-lg-0">
+           
+           <div class="mx-auto bg-white rounded-4 text-center shadow-lg">
+                <div class="d-flex mb-6 mx-auto align-items-center justify-content-center bg-white border rounded-pill" >
+                    
+                </div>
+                <h6 class="fs-6 mt-3 fw-bold">Response Submitted</h6>
+                <span class="d-block">Your response is submitted !</span>
+                <button type="button" onClick={()=>{navigate(-1)}} className='btn btn-primary mt-3 mb-4 fw-bold' > Submit new response</button>
+            </div>
+           </div>
+         </div>
+      
+    </div>
+
+       
+    </div>
+  )
+}
+
+export default Submited
